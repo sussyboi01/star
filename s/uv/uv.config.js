400f08f95105291524b7e5674ba02ee8f4d0ead2
sussyboi01 <hexorod0@gmail.com>
2025-12-17 10:02:03 -0500

    Update uv.config.js

--- s/uv/uv.config.js
+++ s/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-	prefix: "/s/ultraviolet/",
+	prefix: "https://cdn.jsdelivr.net/gh/sussyboi01/star@latest/s/ultraviolet/",
 	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/s/uv/uv.handler.js",
-	bundle: "/s/uv/uv.bundle.js",
-	config: "/s/uv/uv.config.js",
-	sw: "/s/uv/uv.sw.js",
+	handler: "https://cdn.jsdelivr.net/gh/sussyboi01/star@latest/s/uv/uv.handler.js",
+	bundle: "https://cdn.jsdelivr.net/gh/sussyboi01/star@latest/s/uv/uv.bundle.js",
+	config: "https://cdn.jsdelivr.net/gh/sussyboi01/star@latest/s/uv/uv.config.js",
+	sw: "https://cdn.jsdelivr.net/gh/sussyboi01/star@latest/s/uv/uv.sw.js",
 };
